8910fa2bfbf092a776b318bf406301cacc757b26
Tomke123 <kechen20100205@hotmail.com>
2026-04-18 18:38:32 +0800

    chore: remove obsolete development scripts and test utilities

--- hardcode_db.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('backend/database/db.go', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = re.sub(r'\s*"strings"', '', content)
-
-with open('backend/database/db.go', 'w', encoding='utf-8') as f:
-    f.write(content)
